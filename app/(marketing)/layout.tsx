6ae6f73e0b16272c0162075356f984253b852033
firatgoktepe <firatgoktepe1986@gmail.com>
2024-07-05 15:56:26 +0300

    feat: navbar and footer added into layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,12 @@
+import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/footer";
+
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
-      {/* Navbar */}
+      <Navbar />
       <main className="pt-40 bg-slate-100">{children}</main>
-      {/* Footer */}
+      <Footer />
     </div>
   );
 };
